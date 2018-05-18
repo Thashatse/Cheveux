248178a98e0c544df93edb29a60e7b4edb00977c
Thashatse <31445434+Thashatse@users.noreply.github.com>
2018-05-18 13:32:58 +0200

    Revert "Updates To Database, Typelibary, DAL, BLL, Login & Error Page"
    This reverts commit e7e46921d1f3d1dd5c3f626d54a821134fff973b.

--- Stored Procedure/SP_CheckForUser.sql
+++ Stored Procedure/SP_CheckForUser.sql
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
 SELECT COUNT(*) as 'Exists'
 	From CUSTOMER
+=======
+SELECT COUNT(*) as 'Exists'
+	From CUSTOMER
+>>>>>>> parent of e7e4692... Updates To Database, Typelibary, DAL, BLL, Login & Error Page
 	Where CustomerID = '100155997350724101850'
