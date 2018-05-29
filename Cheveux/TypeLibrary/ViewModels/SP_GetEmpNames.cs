5bf633c76eceff1abe84a2bc6ca643b580b4fcf1
Sisikelelwe Maqabangqa <sakes97@gmail.com>
2018-05-30 01:56:10 +0200

    Add Get agenda and employee names stored procedures

---
+++ Cheveux/TypeLibrary/ViewModels/SP_GetEmpNames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeLibrary.ViewModels
+{
+    public class SP_GetEmpNames
+    {
+        public string EmployeeID { get; set; }
+        public string Name { get; set; }
+    }
+}
