c35d8e79e7a626e207e6f90cdc001df183056c33
Indie-Zoe <36884589+Indie-Zoe@users.noreply.github.com>
2018-06-04 22:26:17 +0200

    Update IDBAccess.cs

--- Cheveux/DAL/IDBAccess.cs
+++ Cheveux/DAL/IDBAccess.cs
@@ -33,7 +33,7 @@ namespace DAL
         bool UpdateCustVisit(CUST_VISIT visit);
         bool CreateCustVisit(CUST_VISIT cust_visit);
         bool AddBooking(BOOKING addBooking);
-        List<SP_GetServices> BLL_GetAllServices();
-        List<SP_GetStylists> BLL_GetStylistsForService(string serviceID);
+        List<SP_GetServices> GetAllServices();
+        List<SP_GetStylists> GetStylistsForService(string serviceID);
     }
 }
