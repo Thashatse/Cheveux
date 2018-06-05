2c065ba763c71f97970245885804739e204ed7de
Indie-Zoe <36884589+Indie-Zoe@users.noreply.github.com>
2018-06-05 11:45:07 +0200

    Add files via upload
    Has zero substance

---
+++ Cheveux/Cheveux/MakeABooking.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BLL;
+using TypeLibrary.Models;
+using TypeLibrary.ViewModels;
+
+namespace Cheveux
+{
+    public partial class MakeABooking : System.Web.UI.Page
+    {
+        Functions function = new Functions();
+        IDBHandler handler = new DBHandler();
+        HttpCookie cookie = null;
+        List<SP_GetServices> getAllServices = null;
+        List<SP_GetStylists> getStylists = null;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Check if the user is logged in
+            cookie = Request.Cookies["CheveuxUserID"];
+
+        }
+        public void btnStylist(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
