e060d571a9d202f5a04b021f2ce80f54a26090a9
Thashatse <31445434+Thashatse@users.noreply.github.com>
2018-06-05 12:27:29 +0200

    Update

--- Cheveux/BLL/IDBHandler.cs
+++ Cheveux/BLL/IDBHandler.cs
@@ -32,8 +32,10 @@ namespace BLL
         SP_ViewCustVisit BLL_ViewCustVisit(string customerID, string bookingID);
         bool BLL_UpdateCustVisit(CUST_VISIT visit);
         bool BLL_CreateCustVisit(CUST_VISIT cust_visit);
-        bool BLL_AddBooking(BOOKING addBooking);
-        List<SP_GetServices> BLL_GetAllServices();
-        List<SP_GetStylists> BLL_GetStylistsForService(string serviceID);
+        /*bool BLL_AddBooking(BOOKING addBooking);
+
+       List<SP_GetServices> BLL_GetAllServices();
+       List<SP_GetStylists> BLL_GetStylistsForService(string serviceID);
+       */
     }
 }
