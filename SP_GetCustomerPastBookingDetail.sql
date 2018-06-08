1b65cdfe933e34cea4a344d5904156d173443918
Thashatse <31445434+Thashatse@users.noreply.github.com>
2018-06-08 15:39:22 +0200

    updates

---
+++ SP_GetCustomerPastBookingDetail.sql
@@ -0,0 +1,7 @@
+Select P.[Name], P.ProductDescription, P.Price,  U.FirstName, B.[Date], TS.StartTime, BookingID, B.Arrived       
+	From BOOKING B, TIMESLOT TS, [User] U, PRODUCT P
+	Where BookingID = '1' 
+	AND B.SlotNo = TS.SlotNo 
+	AND B.StylistID = U.UserID 
+	AND B.ServiceID = P.ProductID 
+	AND (B.Arrived = 'Y' Or B.Arrived = 'N')
