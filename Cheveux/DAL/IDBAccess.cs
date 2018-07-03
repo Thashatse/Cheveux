371ef3daf47ff7246fb72609be52c15988ea255d
Thashatse <31445434+Thashatse@users.noreply.github.com>
2018-07-03 13:44:16 +0200

    Revert "Revert "Manage products page""
    This reverts commit de1f86fda6110f09ccb771fec70d3b417300075a.

--- Cheveux/DAL/IDBAccess.cs
+++ Cheveux/DAL/IDBAccess.cs
@@ -50,6 +50,8 @@ namespace DAL
         SP_ViewStylistSpecialisation viewStylistSpecialisation(string empID);
         List<SP_ViewEmployee> viewAllEmployees();
         List<SP_GetEmployeeTypes> getEmpTypes();
+        List<PRODUCT> getAllProducts();
+        List<SP_GetProductTypes> getProductTypes();
 
         /*        bool AddBooking(BOOKING addBooking);
                 List<SP_GetServices> GetAllServices();
