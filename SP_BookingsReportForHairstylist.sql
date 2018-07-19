79762de51d298b18f4eb1722218e53e9bd7009ff
Lachea <36761398+Lachea@users.noreply.github.com>
2018-07-19 15:26:36 +0200

    Add files via upload

---
+++ SP_BookingsReportForHairstylist.sql
@@ -0,0 +1,21 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Description:	Bookings report for hairstylist
+-- =============================================
+CREATE PROCEDURE SP_BookingsReportForHairstylist
+	@stylistID nchar(30)
+AS
+BEGIN
+
+Select BookingID, TIMESLOT.SlotNo, StartTime,EndTime, CustomerID,FirstName,LastName, StylistID, [SERVICE].ServiceID,PRODUCT.Name, Date, Available, Arrived, Comment 
+From BOOKING, TIMESLOT, [USER], [SERVICE] , [PRODUCT]
+where TIMESLOT.SlotNo = BOOKING.SlotNo
+And BOOKING.CustomerID = [USER].UserID
+And BOOKING.ServiceID = [SERVICE].ServiceID
+And [SERVICE].ServiceID = PRODUCT.ProductID
+and BOOKING.StylistID =  @stylistID
+END
+GO
