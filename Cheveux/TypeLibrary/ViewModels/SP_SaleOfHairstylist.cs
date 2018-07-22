889d68d8bc75e411fb5fb6373bdf1c7c73aba2ba
Lachea <36761398+Lachea@users.noreply.github.com>
2018-07-22 17:50:14 +0200

    Add files via upload

---
+++ Cheveux/TypeLibrary/ViewModels/SP_SaleOfHairstylist.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeLibrary.ViewModels
+{
+    public class SP_SaleOfHairstylist
+    {
+        public string SaleID { get; set; }
+        public DateTime date { get; set; }
+        public string CustomerID { get; set; }
+        public char paymentType { get; set; }
+        public string stylistID { get; set; }
+        public string serviceID { get; set; }
+        public char Available { get; set; }
+        public char Arrived { get; set; }
+        public string Comment { get; set; }
+        
+    }
+}
