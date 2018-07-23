16ad0fd6dc4a5bfad1a30807d622b4042aede86f
Indie-Zoe <36884589+Indie-Zoe@users.noreply.github.com>
2018-07-23 19:24:21 +0200

    Add files via upload

---
+++ Cheveux/TypeLibrary/ViewModels/SP_GetTimeSlots.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeLibrary.ViewModels
+{
+    public class SP_GetTimeSlots
+    {
+        public string SlotNo { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
