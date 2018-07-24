9f4187af9c4b50e463afc853c3648008ff88b4d3
Indie-Zoe <36884589+Indie-Zoe@users.noreply.github.com>
2018-07-24 11:53:50 +0200

    Add files via upload

---
+++ MakeABooking.aspx
@@ -0,0 +1,83 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPages/Cheveux.Master" AutoEventWireup="true" CodeBehind="MakeABooking.aspx.cs" Inherits="Cheveux.MakeABooking" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="PageTitle" runat="server">
+    Profile - Cheveux
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <form id="MakeABooking" runat="server">
+        <div class="container-fluid">
+            <div class="container">
+                <div class="col-12">
+                    <div class="jumbotron">
+                        <h1>Make A Booking</h1>
+                        <br />
+                        <asp:Label runat="server" ID="BookingSummary"></asp:Label>
+                    </div>
+                    <div class="container" runat="server" id="divServices" visible="true">
+                        <h3>Choose Service(s)</h3>
+                        <br />
+                        <asp:DropDownList runat="server" ID="drpPickAService">
+                        </asp:DropDownList>
+                    </div>
+
+                    <div class="container" runat="server" id="divStylist" visible="false">
+                        <h3>Choose A Hairstylist</h3>
+                        <br />
+                        <asp:RadioButtonList runat="server" ID="rblPickAStylist">
+                        </asp:RadioButtonList>
+                    </div>
+
+                    <div class="container" runat="server" id="divDateTime" visible="false">
+                        <h3>Choose A Date & Time</h3>
+                        <br />
+                        <asp:Calendar runat="server" ID="calBooking" Width="500" Height="500"></asp:Calendar>
+                        <asp:PlaceHolder runat="server" ID="AvailableTimes">
+                            <asp:DropDownList runat="server" ID="drpAvailableTimes">
+                            </asp:DropDownList>
+                        </asp:PlaceHolder>
+                    </div>
+
+                    <div class="container" runat="server" id="divSummary" visible="false">
+                        <h3>Booking Summary</h3>
+                        <br />
+                        <asp:Table ID="tblBookingSummary" runat="server">
+                            <asp:TableRow ID="Summary" runat="server">
+                                <asp:TableCell Text="Service(s):"></asp:TableCell>
+                                <asp:TableCell>
+                                    <asp:Label runat="server" ID="lblServices"></asp:Label>
+                                </asp:TableCell>
+                            </asp:TableRow>
+                            <asp:TableRow>
+                                <asp:TableCell Text="Stylist"></asp:TableCell>
+                                <asp:TableCell>
+                                    <asp:Label runat="server" ID="lblStylist"></asp:Label>
+                                </asp:TableCell>
+                            </asp:TableRow>
+                            <asp:TableRow>
+                                <asp:TableCell Text="Date & Time"></asp:TableCell>
+                                <asp:TableCell>
+                                    <asp:Label runat="server" ID="lblDate"></asp:Label>&ensp;&ensp;<asp:Label runat="server" ID="lblTime"></asp:Label>
+                                </asp:TableCell>
+                            </asp:TableRow>
+                        </asp:Table>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="container-fluid">
+            <div class="container">
+                <div class="col-2 text-left">
+                    <asp:Button runat="server" ID="btnPrevious" Visible="false" Text="Choose Service(s)" OnClick="btnPrevious_Click"/>
+                </div>
+                <div class="col-8"></div>
+                <div class="col-2 text-right">
+                    <asp:Button runat="server" ID="btnNext" OnClick="btnNext_Click" Text="Choose Hairstylist" />
+                </div>
+            </div>
+        </div>
+    </form>
+</asp:Content>
---
+++ MakeABooking.aspx.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BLL;
+using TypeLibrary.Models;
+using TypeLibrary.ViewModels;
+
+namespace Cheveux
+{
+    public partial class MakeABooking : System.Web.UI.Page
+    {
+        Functions function = new Functions();
+        IDBHandler handler = new DBHandler();
+        HttpCookie cookie = null;
+        List<SP_GetServices> serviceList = null;
+        List<SP_GetStylists> stylistList = null;
+        List<SP_GetBookedTimes> bookedList = null;
+        List<SP_GetSlotTimes> slotList = null;
+        BOOKING book = null;
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Check if the user is logged 
+            try
+            {
+            cookie = Request.Cookies["CheveuxUserID"];
+            serviceList = handler.BLL_GetAllServices();
+            stylistList = handler.BLL_GetAllStylists();
+            slotList = handler.BLL_GetAllTimeSlots();
+            }
+            catch (Exception err)
+            {
+                function.logAnError("unable to comunicate with the database on Make A Booking page: " +
+                    err);
+                BookingSummary.Text = "Database connection failed. Please try again later";
+                divServices.Visible = false;
+
+
+            }
+
+            try
+            {
+                foreach (SP_GetServices services in serviceList)
+                {
+                    drpPickAService.DataSource = serviceList;
+                    drpPickAService.DataTextField = "Name";
+                    drpPickAService.DataValueField = "ServiceID";
+                    drpPickAService.DataBind();
+                }
+
+
+            }
+            catch(Exception err)
+            {
+                function.logAnError(err.ToString());
+            }
+
+            /**try
+            {
+                foreach(SP_GetServices services in serviceList)
+                {
+                    cblPickAService.DataSource = serviceList;
+                    cblPickAService.DataTextField = "Name";
+                    cblPickAService.DataValueField = "ServiceID";
+                    cblPickAService.DataBind();
+                }
+
+                
+            }
+            catch
+            {
+
+            }**/
+
+        }
+ 
+
+        protected void btnNext_Click(object sender, EventArgs e)
+        {
+            if(btnNext.Text == "Choose Hairstylist")
+            {
+                foreach (SP_GetStylists stylists in stylistList)
+                {
+                    rblPickAStylist.DataSource = stylistList;
+                    rblPickAStylist.DataTextField = "FirstName";
+                    rblPickAStylist.DataValueField = "UserID";
+                    rblPickAStylist.DataBind();
+                }
+                HttpCookie cookie = new HttpCookie("booking");
+                cookie["service"] = drpPickAService.SelectedValue.ToString();
+                /**string selectedService = "";
+                foreach(ListItem item in cblPickAService.Items)
+                {
+                    if (item.Selected)
+                    {
+                        selectedService = item.Value;
+                    }
+                }
+                cookie["service"] = selectedService;**/
+                Response.Cookies.Add(cookie);
+                divServices.Visible = false;
+                divStylist.Visible = true;
+                btnPrevious.Visible = true;
+                btnPrevious.Text = "Choose Service(s)";
+                btnNext.Text = "Choose Date & Time";
+                
+            }
+            else if (btnNext.Text == "Choose Date & Time")
+            {
+                HttpCookie cookie = Request.Cookies["booking"];
+                cookie["stylist"] = rblPickAStylist.SelectedValue.ToString();
+                Response.Cookies.Add(cookie);
+                AvailableTimes.Visible = true;
+                bookedList = handler.BLL_GetBookedStylistTimes(rblPickAStylist.SelectedValue.ToString(), calBooking.SelectedDate);
+                if(bookedList != null)
+                {
+                    foreach(SP_GetBookedTimes booked in bookedList)
+                    {
+                        foreach(SP_GetSlotTimes times in slotList)
+                        {
+                            if(booked.SlotNo != times.SlotNo)
+                            {
+                                drpAvailableTimes.DataSource = slotList;
+                                drpAvailableTimes.DataTextField = "Time";
+                                drpAvailableTimes.DataValueField = "SlotNo";
+                                drpAvailableTimes.DataBind();
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    drpAvailableTimes.DataSource = slotList;
+                    drpAvailableTimes.DataTextField = "Time";
+                    drpAvailableTimes.DataValueField = "SlotNo";
+                    drpAvailableTimes.DataBind();
+                }
+                
+                
+                 
+                
+                divStylist.Visible = false;
+                divDateTime.Visible = true;
+                btnPrevious.Visible = true;
+                btnPrevious.Text = "Choose Hairstylist";
+                btnNext.Text = "Booking Summary";
+            }
+            else if (btnNext.Text == "Booking Summary")
+            {
+                HttpCookie cookie = Request.Cookies["booking"];
+                cookie["date"] = calBooking.SelectedDate.ToString();
+                cookie["time"] = drpAvailableTimes.SelectedValue.ToString();
+                Response.Cookies.Add(cookie);
+                divDateTime.Visible = false;
+                divSummary.Visible = true;
+
+                lblServices.Text = drpPickAService.Text;
+                lblStylist.Text = rblPickAStylist.Text;
+                lblDate.Text = calBooking.SelectedDate.ToString();
+                lblTime.Text = drpAvailableTimes.Text;
+
+                btnPrevious.Visible = true;
+                btnPrevious.Text = "Choose Date & Time";
+                btnNext.Text = "Submit";
+            }
+            else if (btnNext.Text == "Submit")
+            {
+                //Make Booking
+                try
+                {
+                    book = new BOOKING();
+
+                    book.BookingID = function.GenerateRandomBookingID();
+                    book.SlotNo = drpAvailableTimes.SelectedValue;
+                    book.Date = calBooking.SelectedDate;
+                    book.CustomerID = cookie["ID"];
+                    book.ServiceID = drpPickAService.SelectedValue;
+                    book.StylistID = rblPickAStylist.SelectedValue;
+                    book.Available = "N";
+
+                    handler.BLL_AddBooking(book);
+                    Response.Redirect("Default.aspx");
+                }
+                catch (Exception err)
+                {
+                    function.logAnError(err.ToString());
+                }
+
+
+            }
+        }
+
+        protected void btnPrevious_Click(object sender, EventArgs e)
+        {
+            if (btnPrevious.Text == "Choose Service(s)")
+            {
+                divServices.Visible = true;
+                divStylist.Visible = false;
+                btnPrevious.Visible = false;
+                btnNext.Text = "Choose Hairstylist";
+            }
+            else if (btnPrevious.Text == "Choose Hairstylist")
+            {
+                divStylist.Visible = true;
+                divDateTime.Visible = false;
+
+                btnPrevious.Visible = true;
+                btnPrevious.Text = "Choose Service(s)";
+                btnNext.Text = "Choose Date & Time";
+            }
+            else if (btnPrevious.Text == "Choose Date & Time")
+            {
+                divDateTime.Visible = true;
+                divSummary.Visible = false;
+
+                btnPrevious.Visible = true;
+                btnPrevious.Text = "Choose Hairstylist";
+                btnNext.Text = "Choose Date & Time";
+            }
+      
+        }
+    }
+}
