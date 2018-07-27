0a5ad3f7cd827299961f10ec47138a17fdf89a4b
Lachea <36761398+Lachea@users.noreply.github.com>
2018-07-27 09:50:51 +0200

    Add files via upload

---
+++ Stored Procedure/SP_SaleOfHairstylist.sql
@@ -0,0 +1,19 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Description:	Sales for a hairstylist
+-- =============================================
+create PROCEDURE SP_SaleOfHairstylist 
+	@StylistID nchar(30)
+AS
+BEGIN
+Select s.SaleID,s.[Date],(u.FirstName + ' ' + u.LastName) AS[FullName], s.CustomerID, s.BookingID
+From SALE s, [USER] u, SALES_DTL, BOOKING b
+Where s.BookingID = b.BookingID
+And b.StylistID = @StylistID
+And u.UserID = b.CustomerID
+And s.CustomerID = b.CustomerID
+END
+GO
