cd3b4d6420507af1193e86dd24aae2b260c59a4c
Thashatse <31445434+Thashatse@users.noreply.github.com>
2018-07-27 09:47:10 +0200

    Update sp_SALEOFHAIRSTYLIST.sql

--- Stored Procedure/sp_SALEOFHAIRSTYLIST.sql
+++ Stored Procedure/sp_SALEOFHAIRSTYLIST.sql
@@ -1,19 +1,12 @@
 ﻿USE [CHEVEUX]
 GO
-/****** Object:  StoredProcedure [dbo].[SP_SaleOfHairstylist]    Script Date: 2018/07/26 15:23:59 ******/
+/****** Description:	Sales for a hairstylist ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-
-
--- Create date: <Create Date,,>
--- Description:	Sales for a hairstylist
--- =============================================
 CREATE PROCEDURE [dbo].[SP_SaleOfHairstylist]
-	-- Add the parameters for the stored procedure here
 	@StylistID nchar(30)
-	
 AS
 BEGIN
 Select s.SaleID,s.[Date],(u.FirstName + ' ' + u.LastName) AS[FullName], s.CustomerID, s.BookingID
