2c326691a241e2ea8952f666554bdb9275cc9554
Indie-Zoe <36884589+Indie-Zoe@users.noreply.github.com>
2018-08-13 11:15:48 +0200

    Add files via upload

---
+++ Cheveux/TypeLibrary/ViewModels/SP_GetWidth.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeLibrary.ViewModels
+{
+    public class SP_GetWidth
+    {
+        public string WidthID { get; set; }
+        public string Description { get; set; }
+    }
+}
