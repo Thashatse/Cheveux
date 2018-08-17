8214e5ce7c13dd9416ddc4f13056e1eb17e74bb0
Thashatse <31445434+Thashatse@users.noreply.github.com>
2018-08-17 12:37:25 +0200

    Booking Service Model

---
+++ Cheveux/TypeLibrary/Models/BookingService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeLibrary.Models
+{
+    public class BookingService
+    {
+        public string BookingID { get; set; }
+
+        public string ServiceID { get; set; }
+    }
+}
