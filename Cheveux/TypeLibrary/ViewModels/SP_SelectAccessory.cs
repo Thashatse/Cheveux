8f7f5ea0b76bbb573de469129f3811fd9b7e366d
Lachea <36761398+Lachea@users.noreply.github.com>
2018-08-17 10:27:08 +0200

    Add files via upload

---
+++ Cheveux/TypeLibrary/ViewModels/SP_SelectAccessory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeLibrary.ViewModels
+{
+    class SP_SelectAccessory
+    {
+        public string productID { get; set; }
+        public string productName { get; set; }
+        public string productDescription { get; set; }
+        public char productType { get; set; }
+        public string productImage { get; set; }
+        public string brandID { get; set; }
+        public string brandName { get; set; }
+
+
+
+
+    }
+}
