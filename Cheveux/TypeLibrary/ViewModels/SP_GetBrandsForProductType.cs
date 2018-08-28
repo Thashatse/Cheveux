8a559a2a9a11df0565b797733358854468013ff6
Lachea <36761398+Lachea@users.noreply.github.com>
2018-08-28 12:46:15 +0200

    Add files via upload

---
+++ Cheveux/TypeLibrary/ViewModels/SP_GetBrandsForProductType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeLibrary.ViewModels
+{
+    public class SP_GetBrandsForProductType
+    {
+        public string BrandID { get; set; }
+        public string Name { get; set; }
+        public char Type{ get; set; } 
+    }
+}
+
