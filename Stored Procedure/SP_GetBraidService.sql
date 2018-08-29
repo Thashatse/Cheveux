58f27c85ba11185d933ba5e0e6f608ad6b1a0601
Indie-Zoe <36884589+Indie-Zoe@users.noreply.github.com>
2018-08-29 13:20:17 +0200

    Add files via upload

--- Stored Procedure/SP_GetBraidService.sql
+++ Stored Procedure/SP_GetBraidService.sql
@@ -4,7 +4,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE PROCEDURE SP_GetBraidService
+ALTER PROCEDURE SP_GetBraidService
 	@ServiceID nchar(10)
 AS
 BEGIN
@@ -13,7 +13,7 @@ BEGIN
 	SET NOCOUNT ON;
 
     -- Insert statements for procedure here
-	SELECT s.Description, w.Description, l.Description
+	SELECT s.Description AS styleDesc, w.Description AS widthDes, l.Description AS lengthDesc
 	FROM SERVICE ser, PRODUCT p, STYLE s, WIDTH w, LENGTH l, BRAID_SERVICE bs
 	WHERE @ServiceID = p.ProductID AND p.ProductID = ser.ServiceID AND [ProductType(T/A/S)] = 'S'
 		AND ser.ServiceID = bs.ServiceID AND bs.StyleID = s.StyleID AND bs.WidthID = w.WidthID
