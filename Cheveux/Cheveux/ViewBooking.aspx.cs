f7c135b59718c91f8e8ee79c3067da4babf1a74d
Thashatse <31445434+Thashatse@users.noreply.github.com>
2018-09-03 10:23:30 +0200

    Edit Booking Update

--- Cheveux/Cheveux/ViewBooking.aspx.cs
+++ Cheveux/Cheveux/ViewBooking.aspx.cs
@@ -1636,6 +1636,7 @@ namespace Cheveux
                             }
                         }
                     }
+                    slotIndex++;
                 }
             }
             catch (Exception err)
