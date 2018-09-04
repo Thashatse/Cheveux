ea20b04582f93405bb72c752c6be4db7ba435752
Indie-Zoe <36884589+Indie-Zoe@users.noreply.github.com>
2018-09-04 14:31:38 +0200

    new update

--- Cheveux/TypeLibrary/ViewModels/SP_GetService.cs
+++ Cheveux/TypeLibrary/ViewModels/SP_GetService.cs
@@ -8,6 +8,7 @@ namespace TypeLibrary.ViewModels
 {
     public class SP_GetService
     {
+        public string ServiceID { get; set; }
         public string ServiceName { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
