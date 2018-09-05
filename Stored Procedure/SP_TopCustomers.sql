bf83835f23c722cfe24ec030f0f2d40acd38d5e7
Lachea <36761398+Lachea@users.noreply.github.com>
2018-09-05 10:48:18 +0200

    Add files via upload

---
+++ Stored Procedure/SP_TopCustomers.sql
@@ -0,0 +1,23 @@
+USE [CHEVEUX]
+GO
+/****** Object:  StoredProcedure [dbo].[SP_GetTopCustomers]   ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+
+GO
+CREATE PROCEDURE SP_GetTopCustomers
+	
+	@customerID nchar(30),
+	@startDate datetime,
+	@endDate datetime
+AS
+BEGIN
+select COUNT(CustomerID), CustomerID
+from BOOKING
+where BOOKING.BookingID = BOOKING.primaryBookingID
+AND   BOOKING.Date BETWEEN @startDate and @endDate
+group by CustomerID
+
+END
+GO
