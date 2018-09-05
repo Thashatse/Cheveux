f2d42c54a6713b42a8497bd075c0e6ec5becde80
Lachea <36761398+Lachea@users.noreply.github.com>
2018-09-05 13:32:04 +0200

    Add files via upload

---
+++ Cheveux/TypeLibrary/ViewModels/SP_GetTopCustomers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeLibrary.ViewModels
+{
+  public class SP_GetTopCustomerbyBooking
+    {
+        public string CustomerID { get; set; }
+        public int noOfBookings { get; set; }
+        public string CustomerName { get; set; }
+    }
+}
