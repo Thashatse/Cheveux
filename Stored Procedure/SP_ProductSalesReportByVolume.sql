ebe96bfb4d78d0c4bdf3c5bd57795dc18ff3dfb9
Thashatse <31445434+Thashatse@users.noreply.github.com>
2018-10-09 13:13:56 +0200

    Product Sales Report Sp's

---
+++ Stored Procedure/SP_ProductSalesReportByVolume.sql
@@ -0,0 +1,22 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Description:	Get the product sales order by Volume
+-- =============================================
+CREATE PROCEDURE SP_ProductSalesReportByVolume 
+	@StartDate DateTime,
+	@EndDate DateTime
+AS
+BEGIN
+	SELECT sum(sd.Price) as [Value], sum(sd.Qty) as Volume, sd.ProductID, max(p.[Name]) as [Product]
+  FROM [CHEVEUX].[dbo].[SALES_DTL] sd, PRODUCT p, SALE s
+  Where p.ProductID = sd.ProductID
+	AND sd.SaleID = s.SaleID
+	AND p.[ProductType(T/A/S)] != 'S'
+	AND s.[Date]  between @StartDate and @EndDate
+  Group by sd.ProductID
+  order by [Volume] desc
+END
+GO
